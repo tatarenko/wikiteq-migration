e8ecb22138763c21b6efd57c76c417d46f2bf7b8
root <vasily@tatarenko.me>
2025-07-26 21:49:44 +0000

    fixing script, adding crontab

--- cron-create-mysql-dump.sh
+++ cron-create-mysql-dump.sh
@@ -34,18 +34,10 @@ if [ -d "$result_dirname" ]; then
     mkdir -p "${result_dirname}" || { echo "can't create non-existent dir ${result_dirname}! aborting..."; exit 1; }
 fi
 
-if [ -f "${result_dirname}/${result_filename}.2" ]; then
-    mv "${result_dirname}/${result_filename}.2" "${result_dirname}/${result_filename}.3"
-fi
-
-if [ -f "${result_dirname}/${result_filename}.1" ]; then
-    mv "${result_dirname}/${result_filename}.1" "${result_dirname}/${result_filename}.2"
-fi
-
 if [ -f "${result_dirname}/${result_filename}" ]; then
-    mv "${result_dirname}/${result_filename}" "${result_dirname}/${result_filename}.1"
+    ctime=`stat --format '%Y' ${result_dirname}/${result_filename}`
+    mv "${result_dirname}/${result_filename}" "${result_dirname}/${result_filename}.${ctime}"
+    find ${result_dirname} -name "${result_filename}.*" | tail -n +4 | xargs -I %% rm %%
 fi
 
 docker-compose --project-directory="${docker_path}" exec -T db /bin/bash -c 'mysqldump --all-databases --triggers --routines --events --single-transaction --skip-lock-tables --column-statistics -uroot -p"$MYSQL_ROOT_PASSWORD"' | gzip  > "${result_path}"
-
-
